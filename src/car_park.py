e47a79887075251c66ba8e01c290b48f25f6f954
Jasmine Sullivan <20147180@tafe.wa.edu.au>
2025-11-05 20:55:50 +0800

    feature: add CarPark class

--- src/car_park.py
+++ src/car_park.py
@@ -1,2 +1,28 @@
 class CarPark:
-    pass
+    def __init__ (self, location="Unknown", capacity=0, plates=None, displays=None):
+        """
+        Initializes a new CarPark object.
+        
+        Args:
+            location (str): Name or address of the car park. Default is "Unknown".
+            capacity (int): Total number of parking bays. Default is 0.
+            plates (list of str, optional): List to store license plates. Default is empty list.
+            displays (list of Display, optional): List of Display objects. Default is empty list.
+        """
+        self.location = location
+        self.display = displays or []
+        self.capacity = capacity
+        self.plate = plates or []
+        
+    def __str__ (self):
+        """
+        Returns a readable string representation of the CarPark object.
+        """
+        return f"The carpark at {self.location} has {self.capacity} bays"
+        
+# testing inputs
+if __name__ == "__main__":
+    location = "Kalamunda"
+    capacity = 15
+    carpark = CarPark(location, capacity)
+    print(carpark)
